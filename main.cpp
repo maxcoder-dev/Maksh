b07e0e29bbc33a5f30626053a3e6384714d0e10a
Max <maxchesscoder@gmail.com>
2025-09-26 21:05:20 +0300

    Add "date" command
    Added a date command.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <string>
+#include <ctime>
 
 using namespace std;
 
-
 int main() {
     string username, token, version = "Maksh 1.0";
-    
+
     cout << "Welcome to the Maksh! Enter your username: ";
     getline(cin, username);
 
@@ -17,11 +17,11 @@ int main() {
         getline(cin, token);
 
         if (token == "whoami") {
-            cout << username <<endl;
+            cout << username << endl;
         } else if (token == "info") {
             cout << "Maksh version " << version << endl;
         } else if (token == "help") {
-            cout << "info, help, whoami, echo your_text, hi, lol, exit " << endl;
+            cout << "info, help, whoami, echo your_text, hi, lol, date, exit" << endl;
         } else if (token == "exit") {
             cout << "Okay, good luck, " << username << endl;
             break;
@@ -29,11 +29,16 @@ int main() {
             cout << "Lol" << endl;
         } else if (token == "hi") {
             cout << "Hello, " << username << endl;
-        } else if (token.rfind("echo ", 0) == 0) { //Echo command
-            string text = token.substr(5); 
+        } else if (token == "date") { 
+            time_t now = time(0);
+            tm *ltm = localtime(&now);
+            char buffer[11];
+            strftime(buffer, sizeof(buffer), "%Y-%m-%d", ltm);
+            cout << buffer << endl;
+        } else if (token.rfind("echo ", 0) == 0) {
+            string text = token.substr(5);
             cout << text << endl;
-        }
-        else {
+        } else {
             cout << "Maksh: command not found, try 'help'" << endl;
         }
     }
